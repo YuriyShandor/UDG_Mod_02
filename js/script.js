b61cd11eabff1756d6b09027979a90513575096c
Yuriy Shandor <yuriyshandor@gmail.com>
2018-01-17 18:23:35 +0200

    Add Header. Mod_04

--- js/script.js
+++ js/script.js
@@ -15,4 +15,17 @@ $(document).ready(function() {
   });
 
   $('select').niceSelect();
+
+  $(".search-box__icon").click(function() {
+    $(".search-box__content").addClass("search-box__content_show");
+  });
+  $(".close-search").click(function() {
+    $(".search-box__content").removeClass("search-box__content_show");
+  });
+
+  $(".toggler-icon").click(function() {
+    $(this).toggleClass("toggler-icon_close");
+    $(".mask").toggleClass("mask_show");
+    $(".nav-bar").toggleClass("nav-bar_show");
+  })
 });
